1c6f5b5ec94775095f6555822bd7a2f42d75ec41
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-08-10 11:26:06 +0300

    Add project files.

---
+++ Users.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[UserId] INT NOT NULL PRIMARY KEY, 
+	[UserName] NCHAR(50) NULL, 
+	[Password] NCHAR(10) NULL, 
+	[Email] NCHAR(30) NULL, 
+	[CreatedDate] DATE NULL
+)
+
+SELECT username FROM users WHERE created_at >= CURRENT_TIMESTAMP - INTERVAL '30 days' AND staged = false
+
+SELECT EMAIL_COLUMN_NAME
+FROM TABLE_NAME
+WHERE EMAIL_COLUMN_NAME LIKE '%@yahoomail.com' OR 
+	  EMAIL_COLUMN_NAME LIKE '%@accelerator.com' OR 
+	  EMAIL_COLUMN_NAME LIKE '%@learning.com' OR
+	  EMAIL_COLUMN_NAME LIKE '%@cloud.vintage.com'
